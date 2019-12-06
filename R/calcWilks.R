79dd89864055aa4ea13eed0ae4e6c0d120a6ad79
Matthew Parker <mrparker909@gmail.com>
2019-12-06 13:37:18 -0800

    - removed necessity for X and Y matrices to be named "X" and "Y", now extracted via lm$model context

--- R/calcWilks.R
+++ R/calcWilks.R
@@ -5,7 +5,7 @@ calcWilks <- function(E,H) {
 }
 
 #' @title wilksLambdaTest
-#' @description Performs Wilks likelihood ratio test on the multivariate regression model lm_mod versus the null hypothesis model lm_nullmod. Note that if n_adj=0 (the default) then the return values pval and pvalAdj will be the same. Otherwise, pval will be the unadjusted pvalue (full degrees of freedom), and pvalAdj will be the adjusted pvalue (reduced degrees of freedom). Data matrices should be named X for the covariates, and Y for the responses.
+#' @description Performs Wilks likelihood ratio test on the multivariate regression model lm_mod versus the null hypothesis model lm_nullmod. Note that if n_adj=0 (the default) then the return values pval and pvalAdj will be the same. Otherwise, pval will be the unadjusted pvalue (full degrees of freedom), and pvalAdj will be the adjusted pvalue (reduced degrees of freedom).
 #' @param lm_mod fitted model (eg: lm(Y~X)) corresponding to the alternative hypothesis.
 #' @param lm_nullmod fitted model (eg: lm(Y~1)) corresponding to the null hypothesis, default is lm(Y~1) when lm_nullmod==NULL.
 #' @param n_adj default=0, subtracted from the error degrees of freedom (for example, may be used to correct for previous testing done on the data).
@@ -14,18 +14,23 @@ calcWilks <- function(E,H) {
 #' x2 = rnorm(500)
 #' y1 = rnorm(500)
 #' y2 = rnorm(500)
-#' Y = cbind(y1,y2)
-#' X = cbind(x1,x2)
-#' wilksLambdaTest(lm(Y~X), n_adj=100)
+#' A = cbind(y1,y2)
+#' B = cbind(x1,x2)
+#' wilksLambdaTest(lm(A~B), n_adj=100)
 #' @export
 wilksLambdaTest <- function(lm_mod, lm_nullmod=NULL, n_adj=0) {
-  if(is.null(lm_nullmod)) {
-    lm_nullmod = lm(lm_mod$model$Y~1)
-  }
+  Ymod=lm_mod$model[[names(lm_mod$model)[1]]]
+  Xmod=lm_mod$model[[names(lm_mod$model)[2]]]
   
-  dof  = ncol(lm_mod$model$X) - ncol(lm_nullmod$model$X)
-  if(is.null(lm_nullmod$model$X)) {
-    dof = ncol(lm_mod$model$X)
+  if(is.null(lm_nullmod)) {
+    lm_nullmod = lm(Ymod~1)
+  }
+  Ynullmod=lm_nullmod$model[[names(lm_nullmod$model)[1]]]
+  Xnullmod=lm_nullmod$model[[names(lm_nullmod$model)[2]]]
+  
+  dof  = ncol(Xmod) - ncol(Xnullmod)
+  if(is.null(Xnullmod)) {
+    dof = ncol(Xmod)
   }
   
   testAdj = likelihoodRatioTest(logLikMVR(lm_nullmod, n_adj), 
